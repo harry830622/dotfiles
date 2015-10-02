44337bf8a59823e259cb80f6f3b4016c19ebf6ed
Harry Chang <harry830622@gmail.com>
2015-10-02 22:08:46 +0800

    Add tpm for tmux

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 chmod +x ~/dotfiles/gnome-terminal-cursor-shape.sh
 sudo cp ~/dotfiles/gnome-terminal-cursor-shape.sh /usr/local/bin
