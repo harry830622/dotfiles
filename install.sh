e81fd3915001830e31ff50e8364667621dd7cc93
Harry Chang <harry830622@gmail.com>
2015-10-02 21:59:06 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+chmod +x ~/dotfiles/gnome-terminal-cursor-shape.sh
+sudo cp ~/dotfiles/gnome-terminal-cursor-shape.sh /usr/local/bin
