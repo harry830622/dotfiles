c88fd6d706dd1e878abc245e2715bd7177e188eb
Harry Chang <harry830622@gmail.com>
2015-10-12 09:06:49 +0800

    Move gnome cursor shape to home dir

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-sudo cp ~/dotfiles/gnome-terminal-cursor-shape.sh /usr/local/bin
-sudo chmod +x /usr/local/bin/gnome-terminal-cursor-shape.sh
+cp ~/dotfiles/gnome-terminal-cursor-shape.sh ~/.gnome-terminal-cursor-shape.sh
+chmod +x ~/.gnome-terminal-cursor-shape.sh
