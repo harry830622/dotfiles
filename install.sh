dc733270a11eb5fe7d68eebf1d7497fafdfa1f5f
Harry Chang <harry830622@gmail.com>
2015-11-07 12:34:42 +0800

    Ignore vim temp files in git

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore ~/.gitignore
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
