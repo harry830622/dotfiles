f15f0435245c5cb7d27953a60ffcea22e1a41b52
Harry Chang <harry830622@gmail.com>
2017-11-02 19:42:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,26 +4,41 @@ dot_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo -e "dot_dir=$dot_dir\nexport dot_dir" > ~/.myenv
 
-if [[ ! -d ~/.zprezto ]]; then
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+read -r -p "Install zsh config? [Y/n] "
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  if [[ ! -d ~/.zprezto ]]; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+  fi
+  ln -sf "$dot_dir/zsh/zpreztorc" ~/.zpreztorc
+  ln -sf "$dot_dir/zsh/zshrc" ~/.zshrc
 fi
 
-if [[ ! -d ~/.vim ]]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+read -r -p "Install vim config? [Y/n] "
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  if [[ ! -d ~/.vim ]]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  fi
+  ln -sf "$dot_dir/vim/vimrc" ~/.vimrc
 fi
 
-if [[ ! -d ~/.tmux ]]; then
-  git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+read -r -p "Install tmux config? [Y/n] "
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  if [[ ! -d ~/.tmux ]]; then
+    git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+  fi
+  ln -sf "$dot_dir/tmux/tmux.conf" ~/.tmux.conf
 fi
 
-if [[ ! -f ~/.z.sh ]]; then
-  curl -fLo ~/.z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
+read -r -p "Install git config? [Y/n] "
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  ln -sf "$dot_dir/git/gitconfig" ~/.gitconfig
+  ln -sf "$dot_dir/git/gitignore" ~/.gitignore
 fi
 
-ln -s "$dot_dir/zsh/zpreztorc" ~/.zpreztorc
-ln -s "$dot_dir/zsh/zshrc" ~/.zshrc
-ln -s "$dot_dir/vim/vimrc" ~/.vimrc
-ln -s "$dot_dir/tmux/tmux.conf" ~/.tmux.conf
-ln -s "$dot_dir/git/gitconfig" ~/.gitconfig
-ln -s "$dot_dir/git/gitignore" ~/.gitignore
+read -r -p "Install utils? [Y/n] "
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  if [[ ! -f ~/.z.sh ]]; then
+    curl -fLo ~/.z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
+  fi
+fi
